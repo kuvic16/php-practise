124db5f29590efb7896b7a6005fb56fa83aba26b
Shaiful Islam <kuvic16@gmail.com>
2020-05-21 16:55:40 +0600

    PHP basics: variables

--- variables.php
+++ variables.php
@@ -12,4 +12,8 @@ echo "Hello, $name";
 echo 'Hello, ' . $name;
 
 // use braces to differentiate the variable in string
-echo "Hello, {$name}";
+echo "Hello, { $name }";
+
+$greeting = 'Hello';
+echo "$greeting, $name";
+echo "{ $greeting }, { $name }";
