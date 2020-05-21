cb3c840b122c9e850793098975d9aa233f1928a1
Shaiful Islam <kuvic16@gmail.com>
2020-05-21 17:30:13 +0600

    separating php and html

---
+++ separate-logic/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$greetings = 'Hello Palash, after 16 years later';
+
+require 'index.view.php';
---
+++ separate-logic/index.view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html langs="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>        
+    </head>
+    <body>
+        <h2><?= $greetings ?></h2>
+    </body>
+</html>
