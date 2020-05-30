e6a8e911a44c3e868a2198462ddeb4077f1cc0ff
Shaiful Islam <kuvic16@gmail.com>
2020-05-30 16:44:21 +0600

    config file added

--- pdo/database/Connection.php
+++ pdo/database/Connection.php
@@ -10,10 +10,15 @@ class Connection
      * 
      * @return PDO
      */
-    public static function make()
+    public static function make($config)
     {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '1234');
+            return new PDO(
+                $config['connection'] . ';dbname=' . $config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
         } catch (PDOException $e) {
             die($e->getMessage());
         }
